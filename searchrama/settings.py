7b15616a3ca273ea29b26994566488fe3f1e0f30
zen-40 <41286833+zen-40@users.noreply.github.com>
2018-10-01 17:06:34 +0200

    Update settings.py

--- searchrama/settings.py
+++ searchrama/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@jna4h_osbh3ote!d(=_-i+(ef+)c7v0z)q&d9unu39a8x1l1!'
+SECRET_KEY = 'XXX'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
